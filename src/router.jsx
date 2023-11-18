ed3f7952b43fb09952a52bf244768f02c656aaf8
miqbalfr12 <miqbalfr12@gmail.com>
2023-11-18 13:42:43 +0700

    refactor(route): change homepage for landing page

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,14 @@
 import Counter from "./Pages/Counter";
 import App from "./App";
+import Landing from "./Pages/Landing";
 
 export const Router = [
   {
     path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/all",
     element: <App />,
   },
   {
